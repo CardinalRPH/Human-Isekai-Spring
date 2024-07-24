87c06df28cd2995a4a31f42c9257c4688b24f504
CardinalRPH <rayhanfebriyan62@gmail.com>
2024-07-25 02:48:50 +0700

    Initial commit

---
+++ src/main/java/com/cardinal/project/human_isekai_transfer/HumanIsekaiTransferApplication.java
@@ -0,0 +1,13 @@
+package com.cardinal.project.human_isekai_transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanIsekaiTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanIsekaiTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardinal/project/human_isekai_transfer/HumanIsekaiTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardinal.project.human_isekai_transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanIsekaiTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
